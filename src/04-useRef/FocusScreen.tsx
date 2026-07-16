bdc8c8633ebe2232ac0b46451b0ebb01577bff6b
José Miguel Pérez Buenaño <elsemi6@gmail.com>
2026-07-16 15:06:03 +0200

    autofocus

---
+++ src/04-useRef/FocusScreen.tsx
@@ -0,0 +1,31 @@
+import { useRef } from "react"
+
+export const FocusScreen = () => {
+    // Initial value null react syncrony
+    const inputRef = useRef<HTMLInputElement>(null);
+
+    const handleClick = () => {
+        console.log(inputRef.current?.value);
+        inputRef.current?.select(); // you can use it to reset a password
+        // inputRef.current?.select();
+    }
+
+    return (
+        <div className="bg-gradient flex flex-col gap-4">
+            <h1 className="text-2xl font-light text-white">Focus screen</h1>
+
+            <input
+                ref={inputRef}
+                type="text"
+                className="bg-white text-black px-4 py-2 rounded-md"
+                autoFocus
+            />
+
+            <button
+                className="bg-blue-500 text-white px-4 py-2 rounded-md cursor-pointer"
+                onClick={handleClick}>
+                Set Focus
+            </button>
+        </div>
+    )
+}
--- src/main.tsx
+++ src/main.tsx
@@ -5,10 +5,11 @@ import './index.css'
 // import { TrafficLight } from './01-useState/TrafficLight'
 // import { TrafficLightWithEffect } from './02-useEffect/TrafficLightWithEffect'
 // import { TrafficLightWithHook } from './02-useEffect/TrafficLightWithHook'
-import { PokemonPage } from './03-examples/PokemonPage'
+// import { PokemonPage } from './03-examples/PokemonPage'
+import { FocusScreen } from './04-useRef/FocusScreen'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <PokemonPage />
+    <FocusScreen />
   </StrictMode>
 )
