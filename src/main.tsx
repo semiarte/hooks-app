c0e9909897295ee810a639c9b4a31f136eb53a85
José Miguel Pérez Buenaño <elsemi6@gmail.com>
2026-07-16 12:12:12 +0200

    TrafficLights with effect

--- src/main.tsx
+++ src/main.tsx
@@ -2,10 +2,11 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 // import { HooksApp } from './HooksApp'
-import { TrafficLight } from './01-useState/TrafficLight'
+// import { TrafficLight } from './01-useState/TrafficLight'
+import { TrafficLightWithEffect } from './02-useEffect/TrafficLightWithEffect'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <TrafficLight />
+    <TrafficLightWithEffect />
   </StrictMode>
 )
