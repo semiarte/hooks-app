aaf7ef96e9028a8de89eb6a67ba597963aacdf72
José Miguel Pérez Buenaño <elsemi6@gmail.com>
2026-07-16 13:29:17 +0200

    useTrafficLights custom hook

--- src/02-useEffect/TrafficLightWithHook.tsx
+++ src/02-useEffect/TrafficLightWithHook.tsx
@@ -1,71 +1,27 @@
-import { useEffect, useState } from "react";
+import { useTrafficLight } from "./hooks/useTrafficLight";
 
-const colors = {
-    red: 'bg-red-500 animate-pulse',
-    yellow: 'bg-yellow-500 animate-pulse',
-    green: 'bg-green-500 animate-pulse',
-}
+export const TrafficLightWithHook = () => {
 
-// for primitives
-type TrafficLightColor = keyof typeof colors;
-
-export const TrafficLightWithEffect = () => {
-
-    const [light, setLight] = useState<TrafficLightColor>('red');
-    const [countdown, setCountdown] = useState(5)
-
-    // countDown effect
-    useEffect(() => {
-        if (countdown === 0) return;
-
-        const intervalId = setInterval(() => {
-            setCountdown(prev => prev - 1);
-        }, 1000);
-
-        return () => {
-            clearInterval(intervalId);
-        }
-
-    }, [countdown]);
-
-    // change light color effect
-    useEffect(() => {
-        if (countdown === 0) {
-            setCountdown(5);
-            if (light === 'red') {
-                setLight('green');
-                return;
-            }
-            if (light === 'yellow') {
-                setLight('red');
-                return;
-            }
-            if (light === 'green') {
-                setLight('yellow');
-                return;
-            }
-        }
-        return;
-    }, [countdown, light]);
+    const { countdown, percentage, redLight, yellowLight, greenLight } = useTrafficLight();
 
     return (
-        <div className="min-h-screen bg-gradient-to-br from-slate-900 via-gray-900 to-slate-800 flex items-center justify-center p-4">
+        <div className="min-h-screen bg-linear-to-br from-slate-900 via-gray-900 to-slate-800 flex items-center justify-center p-4">
             <div className="flex flex-col items-center space-y-8">
                 <h1 className="text-white text-3xl font-light">Semáforo con useEffect</h1>
                 <h2 className="text-white text-xl">{countdown}</h2>
 
                 <div className="w-64 bg-gray-700 rounded-full h-2">
                     <div
-                        style={{ width: `${(countdown / 5) * 100}%` }}
+                        style={{ width: `${percentage}%` }}
                         className="bg-blue-500 h-2 rounded-full transition-all duration-1000 ease-linear"
                     ></div>
                 </div>
 
-                <div className={`w-32 h-32 ${light === 'red' ? colors[light] : 'bg-gray-500'} rounded-full`}></div>
+                <div className={`w-32 h-32 ${redLight} rounded-full`}></div>
 
-                <div className={`w-32 h-32 ${light === 'yellow' ? colors[light] : 'bg-gray-500'} rounded-full`}></div>
+                <div className={`w-32 h-32 ${yellowLight} rounded-full`}></div>
 
-                <div className={`w-32 h-32 ${light === 'green' ? colors[light] : 'bg-gray-500'} rounded-full`}></div>
+                <div className={`w-32 h-32 ${greenLight} rounded-full`}></div>
             </div>
         </div>
     );
--- src/main.tsx
+++ src/main.tsx
@@ -3,10 +3,11 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 // import { HooksApp } from './HooksApp'
 // import { TrafficLight } from './01-useState/TrafficLight'
-import { TrafficLightWithEffect } from './02-useEffect/TrafficLightWithEffect'
+// import { TrafficLightWithEffect } from './02-useEffect/TrafficLightWithEffect'
+import { TrafficLightWithHook } from './02-useEffect/TrafficLightWithHook'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <TrafficLightWithEffect />
+    <TrafficLightWithHook />
   </StrictMode>
 )
